fc849bc03f96d5718bd1c3df93d08da2ba71d6f7
Khaled Hassan <132917415+KhaledHassangit@users.noreply.github.com>
2024-09-03 14:19:48 +0300

    Backend update

--- E-Shop Front End/src/Api/BaseURL.js
+++ E-Shop Front End/src/Api/BaseURL.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 
 
-const baseURL = axios.create({baseURL:"http://127.0.0.1:8000"})
+// const baseURL = axios.create({baseURL:"http://127.0.0.1:8000"})
+const baseURL = axios.create({baseURL:"https://backend-for-ecommerce-plateform2.onrender.com"})
 export default baseURL
